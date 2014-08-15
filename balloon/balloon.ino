3ed4a23e9a58239876e74f3aa267b4b6bbc05da9
James Tautges <jamesdtautges@gmail.com>
2014-08-15 12:53:37 -0500

    Changed balloon.ino to accomodate new sensor interfaces

--- balloon/balloon.ino
+++ balloon/balloon.ino
@@ -4,9 +4,6 @@
 #include "adxl335.h"
 
 T5403 barometer (MODE_I2C);
-double barometerTemp, absolutePressure, baselinePressure, deltaAltitude, absoluteAltitude;
-const double baseAltitude = 270.0; // need to get more accurate altitude value at launch site
-
 ML8511 uvsensor (A0, A1);
 
 // ADXL335 accelerometer (A3, A4, A5);
@@ -17,46 +14,19 @@ void setup() {
   Serial.println("serial works");
   uvsensor.begin();
   Serial.println("UV Sensor Works");
-  barometer.begin();
+  barometer.begin(270.0); // initialize barometer with altitude
   Serial.println("Barometer initialized");
-  baselinePressure = barometer.getPressure(MODE_ULTRA);
-  Serial.println("baseline pressure established");
 }
 
-double pressureToAltitude(double P) // Via SparkFun example implementation, since magic numbers are magical
-// Given a pressure measurement P (Pa) and the baseline pressure from setup, return altitude (in meters) above baseline.
-{
-	return(44330.0*(1-pow(P/baselinePressure,1/5.255)));
-}
-
-double getAltitude(void) {
- absolutePressure = barometer.getPressure(MODE_ULTRA);
-  /*
-  MODE          Time
-  
-  MODE_LOW      5ms
-  MODE_STANDARD 11ms
-  MODE_HIGH     19ms
-  MODE_ULTRA    67ms 
-  */
-  
-  deltaAltitude = pressureToAltitude(absolutePressure);
-  absoluteAltitude = baseAltitude + deltaAltitude; 
-}
-
+uint32_t lastTime;
 
 void loop() {
-  delay(500);
+  lastTime = millis();
+  Serial.println(barometer.getAltitude());
+  Serial.println(barometer.read());
   
-  // Barometer measurements
-  barometerTemp = barometer.getTemperature(CELSIUS);
-  // altitude = getAltitude()
-  
-  //Data output
   Serial.println(uvsensor.read());
-  Serial.println(getAltitude());
-//  Serial.print("X raw value: ");
-//  Serial.println(accelerometer.getRawAxis(A5));
-  
+
+  while(millis() - lastTime < 500) {} // better than delaying since this accounts for the time it took to perform the measurements
 }
 
