27bbd9a82bcd294d3c591e5eaf5cba50175dbb1e
James Tautges <jamesdtautges@gmail.com>
2014-08-15 14:24:08 -0500

    Added test code for accelerometer

--- balloon/balloon.ino
+++ balloon/balloon.ino
@@ -5,27 +5,37 @@
 
 T5403 barometer (MODE_I2C);
 ML8511 uvsensor (A0, A1);
-
-// ADXL335 accelerometer (A3, A4, A5);
-// will need to give a way to normalize analog reads to Gs.
+ADXL335 accelerometer (A3, A4, A5);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("serial works");
+  Serial.println("Serial Initialized");
   uvsensor.begin();
-  Serial.println("UV Sensor Works");
+  Serial.println("UV Sensor Initialized");
   barometer.begin(270.0); // initialize barometer with altitude
-  Serial.println("Barometer initialized");
+  Serial.println("Barometer Initialized");
+  accelerometer.begin();
+  Serial.println("Accelerometer Initialized");
 }
 
 uint32_t lastTime;
 
 void loop() {
   lastTime = millis();
-  Serial.println(barometer.getAltitude());
-  Serial.println(barometer.read());
+  Serial.print(barometer.getAltitude());
+  Serial.print(" | ");
+  Serial.print(barometer.read());
+  Serial.print(" || ");
   
-  Serial.println(uvsensor.read());
+  Serial.print(uvsensor.read());
+  Serial.print(" || ");
+
+  acceleration data = accelerometer.read();
+  Serial.print(data.x);
+  Serial.print(" | ");
+  Serial.print(data.y);
+  Serial.print(" | ");
+  Serial.println(data.z);
 
   while(millis() - lastTime < 500) {} // better than delaying since this accounts for the time it took to perform the measurements
 }
