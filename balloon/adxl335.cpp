31adc2c73cefa35dabc7e73ee1a6cd72c9f427cd
James Tautges <jamesdtautges@gmail.com>
2014-08-14 23:06:07 -0500

    Changed function names to camelcase. Really Newt?

--- balloon/adxl335.cpp
+++ balloon/adxl335.cpp
@@ -13,12 +13,12 @@ void ADXL335::begin(void) {
 	pinMode(z, INPUT);
 }
  
-uint16_t ADXL335::getrawaxis(uint8_t axis) {
+uint16_t ADXL335::getRawAxis(uint8_t axis) {
         uint16_t output = analogRead(axis);
         return output;
 }
 
-uint16_t ADXL335::readaxis(char axis) { 
+uint16_t ADXL335::readAxis(char axis) { 
         // The raw values won't give us Gs, which could be more useful.
         uint16_t rawvalue = ADXL335::getrawaxis(axis);
         // Is response linear across the entire range?
--- balloon/adxl335.h
+++ balloon/adxl335.h
@@ -7,8 +7,8 @@ class ADXL335 {
  public:
   ADXL335(uint8_t xaxis, uint8_t yaxis, uint8_t zaxis);
   void begin(void);
-  uint16_t getrawaxis(uint8_t axis);
-  uint16_t readaxis(uint8_t axis);
+  uint16_t getRawAxis(uint8_t axis);
+  uint16_t readAxis(uint8_t axis);
  private:
   uint8_t x;
   uint8_t y;
