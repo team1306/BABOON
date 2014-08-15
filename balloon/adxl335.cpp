5f06e0be430edbad238da2b9857c668996a857b4
James Tautges <jamesdtautges@gmail.com>
2014-08-14 23:19:50 -0500

    Found hidden char and destroyed it

--- balloon/adxl335.cpp
+++ balloon/adxl335.cpp
@@ -18,7 +18,7 @@ uint16_t ADXL335::getRawAxis(uint8_t axis) {
         return output;
 }
 
-uint16_t ADXL335::readAxis(char axis) { 
+uint16_t ADXL335::readAxis(uint8_t axis) { 
         // The raw values won't give us Gs, which could be more useful.
         uint16_t rawvalue = ADXL335::getrawaxis(axis);
         // Is response linear across the entire range?
