4f2c68d07c606dad264700bba5af2db6486df102
James Tautges <jamesdtautges@gmail.com>
2015-08-02 21:22:16 -0500

    Added thermocouple and new CSV format

--- balloon/balloon.ino
+++ balloon/balloon.ino
@@ -7,6 +7,7 @@
 T5403 barometer (MODE_I2C);
 ML8511 uvsensor (A0);
 ADXL335 accelerometer (A1, A2, A3);
+MAX6675 thermocouple (13, 10, 12);
 
 void setup() {
   Serial.begin(9600);
@@ -18,24 +19,32 @@ void setup() {
   Serial.println("Barometer Initialized");
   accelerometer.begin();
   Serial.println("Accelerometer Initialized");
+  thermocouple.readCelsius();
+  Serial.println("Thermocouple Initialized");
+  Serial.println();
+
+  Serial.print("altitude,pressure,uv,x-axis,y-axis,z-axis,temp");
 }
 
 void loop() {
   Serial.print("DATA: ");
 
   Serial.print(barometer.getAltitude());
-  Serial.print(" | ");
+  Serial.print(",");
   Serial.print(barometer.read());
-  Serial.print(" || ");
+  Serial.print(",");
 
   Serial.print(uvsensor.read());
-  Serial.print(" || ");
+  Serial.print(",");
 
   acceleration data = accelerometer.read();
   Serial.print(data.x);
-  Serial.print(" | ");
+  Serial.print(",");
   Serial.print(data.y);
-  Serial.print(" | ");
+  Serial.print(",");
   Serial.print(data.z);
+  Serial.print(",");
+
+  Serial.print(thermocouple.readCelsius());
   Serial.print("\n");
 }
