17c61cdcd2c881b56eb354352d0f34b8da26b138
James Tautges <jamesdtautges@gmail.com>
2015-08-02 21:59:18 -0500

    Added times and mechanics for cutdown

--- balloon/balloon.ino
+++ balloon/balloon.ino
@@ -3,11 +3,16 @@
 #include "t5403.h"
 #include "adxl335.h"
 #include "max6675.h"
+#include "nichrome.h"
 
 T5403 barometer (MODE_I2C);
 ML8511 uvsensor (A0);
 ADXL335 accelerometer (A1, A2, A3);
 MAX6675 thermocouple (13, 10, 12);
+NiChrome wire (9, 1000*60); // second parameter is milliseconds for cutdown (1 minute)
+
+const unsigned long startTime = millis();
+const unsigned long airTime = 1000*60*60*1.5; // total time before cutdown (1.5 hours)
 
 void setup() {
   Serial.begin(9600);
@@ -23,7 +28,7 @@ void setup() {
   Serial.println("Thermocouple Initialized");
   Serial.println();
 
-  Serial.print("altitude,pressure,uv,x-axis,y-axis,z-axis,temp");
+  Serial.print("altitude,pressure,uv,x-axis,y-axis,z-axis,temp,cutting");
 }
 
 void loop() {
@@ -46,5 +51,13 @@ void loop() {
   Serial.print(",");
 
   Serial.print(thermocouple.readCelsius());
+  Serial.print(",");
+
+  if(millis() - startTime > airTime) {
+    Serial.print(wire.cutDown());
+  }
+  else {
+    Serial.print(false);
+  }
   Serial.print("\n");
 }
